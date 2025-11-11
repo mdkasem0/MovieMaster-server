3bb931ab8b75f8ab44b4263cecb001e444baa2e2
mdkasem0 <mkashem2005@gmail.com>
2025-11-11 12:47:24 +0600

    MovieMaster Server initialized

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+      res.send("MovieMaster Pro Backend is running!");
+    });
+
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
