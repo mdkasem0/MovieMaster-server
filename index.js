052397bb989375cfc24659cf073244f78d274aa2
mdkasem0 <mkashem2005@gmail.com>
2025-11-11 12:57:01 +0600

    put & get end-point added for user

--- index.js
+++ index.js
@@ -24,13 +24,43 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-   
+   // 🗃️ Database and Collections
+    const db = client.db("MovieMasterDB");
+    const moviesCollection = db.collection("movies");
+    const watchlistCollection = db.collection("watchlist");
+    const usersCollection = db.collection("users");
     // ✅ Root Route
     app.get("/", (req, res) => {
       res.send("🎬 MovieMaster Pro Backend is running with Users Collection!");
     });
 
+
+     // ✅Create and  Update User
+    app.put("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const userData = req.body;
+
+      const filter = { email };
+      const updateDoc = { $set: userData };
+      const options = { upsert: true };
+
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
  
+     // ✅ Get Single User
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({ email });
+      res.send(user);
+    });
+
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
