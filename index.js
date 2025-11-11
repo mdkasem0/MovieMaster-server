4028137d633b65106f22a968556ee2b84cfbb80e
mdkasem0 <mkashem2005@gmail.com>
2025-11-11 12:53:40 +0600

    MONGODB added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,9 +11,37 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-      res.send("MovieMaster Pro Backend is running!");
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+   
+    // ✅ Root Route
+    app.get("/", (req, res) => {
+      res.send("🎬 MovieMaster Pro Backend is running with Users Collection!");
     });
+
+ 
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 // Start server
